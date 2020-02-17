ccc06bd1cee46943292060f77dcc36c8231d2e78
Inez Martens <61147903+InezMartens@users.noreply.github.com>
2020-02-17 13:34:40 +0100

    updatet with my info

--- data.js
+++ data.js
@@ -10,10 +10,10 @@ const data = {
      */
 
     "general": {
-        "firstName": "Andrei",      // fill in with your own first name
-        "lastName": "Tarkovsky",    // fill in with your own last name
-        "class": "1NMD",            // fill in with your class/group e.g. 1AVDa, 1CMOa, 1NMD, ...
-        "studentnumber": "8675309"  // fill in with your student number
+        "firstName": "Inez",      // fill in with your own first name
+        "lastName": "Martens",    // fill in with your own last name
+        "class": "1AVDa",            // fill in with your class/group e.g. 1AVDa, 1CMOa, 1NMD, ...
+        "studentnumber": "112925"  // fill in with your student number
     },
 
     /**
